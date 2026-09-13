9656324290f0c6f3ca04491495be73163c02facb
Vaishnavi Trivedi <vaishnavitrivedi.connect@gmail.com>
2026-09-13 23:43:18 +0530

    Time: 59 ms (43.05%), Space: 13 MB (68.83%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size();
+        int ans = 0;
+
+        // Try every possible vertical shift
+        for (int dr = -n + 1; dr < n; dr++) {
+
+            // Try every possible horizontal shift
+            for (int dc = -n + 1; dc < n; dc++) {
+
+                int overlap = 0;
+
+                // Check every cell of img1
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+
+                        // Only move the 1s
+                        if (img1[i][j] == 1) {
+
+                            int ni = i + dr;
+                            int nj = j + dc;
+
+                            // Check if moved position is inside matrix
+                            if (ni >= 0 && ni < n &&
+                                nj >= 0 && nj < n) {
+
+                                // Check overlap
+                                if (img2[ni][nj] == 1) {
+                                    overlap++;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                ans = max(ans, overlap);
+            }
+        }
+
+        return ans;
+    }
+};
