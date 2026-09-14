60e5f1a63780c3578003d9ad3439e79d11c68eb3
Vaishnavi Trivedi <vaishnavitrivedi.connect@gmail.com>
2026-09-14 23:59:59 +0530

    Time: 0 ms (100%), Space: 10 MB (11.03%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        
+        // rec1 is completely left of rec2
+        if (rec1[2] <= rec2[0])
+            return false;
+        
+        // rec2 is completely left of rec1
+        if (rec2[2] <= rec1[0])
+            return false;
+        
+        // rec1 is completely below rec2
+        if (rec1[3] <= rec2[1])
+            return false;
+        
+        // rec2 is completely below rec1
+        if (rec2[3] <= rec1[1])
+            return false;
+        
+        return true;
+    }
+};
